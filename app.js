83fcd23eece2cdd6dedb9b80b308f6f0b10db008
ammustafa1 <109623721+ammustafa1@users.noreply.github.com>
2022-10-24 16:43:30 -0400

    Add files via upload
    Inquirer installed, node_modules, package.json, package-lock.json, and index.html files added

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log('Hello Node!');
+const inquirer = require('inquirer');
+//const fs = require('fs');
+//const generatePage = require('./src/page-template');
+
+//const pageHTML = generatePage(name, github);
+
+//fs.writeFile('./index.html', pageHTML, err => {
+//if (err) throw err;
+
+//console.log('Portfolio complete! Check out index.html to see the output!');
+//});
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
